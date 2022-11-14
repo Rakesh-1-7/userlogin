36667c49467a23ad018ff62ff0d489e52524dc3f
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2022-11-14 19:26:53 +0530

    fixed app

--- userlogin/info.py
+++ userlogin/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hoodieguyyy@gmail.com'
-EMAIL_HOST_PASSWORD = 'Raki@1509'
+EMAIL_HOST_PASSWORD = 'P4ssword@12'
 EMAIL_PORT = 587
