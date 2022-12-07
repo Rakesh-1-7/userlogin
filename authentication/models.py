abba3e765fe52e9baf14102bd9d17962e52efcb2
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2022-12-08 00:05:33 +0530

    fileupload

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Filedatabase(models.Model):
+    semester = models.CharField(max_length=255)
+    course = models.CharField(max_length=255)
+    notes = models.CharField(max_length=255)
+    myfiles = models.FileField(upload_to="")
+    display = models.CharField(max_length=255)
